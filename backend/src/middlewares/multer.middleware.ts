332d4f6601563e70df0aa231baa720a8c86ff312
Zakiya <zakiyamanzoor04@gmail.com>
2026-02-02 23:32:09 +0530

    add multer for files

---
+++ backend/src/middlewares/multer.middleware.ts
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }, 
+  fileFilter: (_, file, cb) => {
+    if (!file.mimetype.startsWith("image/")) {
+      cb(new Error("Images only"));
+    }
+    cb(null, true);
+  },
+});
