3b16711605588deb577af944f4b3950be310be8a
Zakiya <zakiyamanzoor04@gmail.com>
2026-02-07 22:58:34 +0530

    add cloudinary fucntion

--- backend/src/utils/cloudinary.ts
+++ backend/src/utils/cloudinary.ts
@@ -6,4 +6,29 @@ cloudinary.config({
    api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
-export default cloudinary
+const uploadOncloudinary = async (buffer:Buffer,mimetype: string):Promise<{url:string, imageUrl: string}>=>{
+  try {
+   const b64 = buffer.toString('base64');
+   if(!b64) throw new Error ("Buffer not found")
+
+   const dataURI = `data:${mimetype};base64,${b64}`
+   const result:any = await cloudinary.uploader.upload(dataURI,{resource_type:"auto"})
+   return {url: result.secure_url, imageUrl: result.puclic_id}
+  } catch (error) {
+
+   throw new Error("Cloudinary Upload Failed!")
+  }
+
+}
+
+const deleteFromCloudinary = async (id:string)=>{
+ try {
+  if(!id) throw new Error ("Image id is not provided!")
+    await cloudinary.uploader.destroy(id)
+ } catch (error) {
+  throw error
+ }
+}
+
+export {uploadOncloudinary, deleteFromCloudinary} 
+
